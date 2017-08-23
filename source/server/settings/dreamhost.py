4a1050858ff8920bfa2df258939f4e9bdea8e80c
Fernando Espíndola <fer.esp@gmail.com>
2017-08-22 23:12:09 -0300

    [Closes #9] Criando o banco de dados no Dreamhost e configurando a aplicação para acessa-lo

--- source/server/settings/dreamhost.py
+++ source/server/settings/dreamhost.py
@@ -3,3 +3,21 @@ from .base import *
 STATIC_ROOT = '/home/fems/web/healthcheck.fe.fernandoe.com/public/static'
 
 ALLOWED_HOSTS = ['healthcheck.fe.fernandoe.com', ]
+
+from configparser import RawConfigParser
+config = RawConfigParser()
+config.read('/home/fems/conf/fe-healthcheck-server.ini')
+
+DATABASES = {
+    'default': {
+        'ENGINE': config.get('database', 'DATABASE_ENGINE'),
+        'NAME': config.get('database', 'DATABASE_NAME'),
+        'USER': config.get('database', 'DATABASE_USER'),
+        'PASSWORD': config.get('database', 'DATABASE_PASSWORD'),
+        'HOST': config.get('database', 'DATABASE_HOST'),
+        'PORT': config.get('database', 'DATABASE_PORT'),
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=MyISAM',
+        }
+    }
+}
