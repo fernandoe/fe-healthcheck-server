820cca2ebcc8668ea93261ed5fa11565551ddca0
Fernando Espíndola <fer.esp@gmail.com>
2017-09-11 12:46:20 -0300

    [Closes #19] Adicionado uma coluna no modelo de Endereco com o último status da verificação

--- source/healthcheck/admin.py
+++ source/healthcheck/admin.py
@@ -7,10 +7,19 @@ from healthcheck.models import Endereco, Verificacao
 class EnderecoModelAdmin(admin.ModelAdmin):
     search_fields = ['url', ]
     list_filter = ['ativo', ]
-    list_display = ['get_uuid', 'url', 'ativo']
+    list_display = ['get_uuid', 'url', 'ativo', 'get_last_status']
+
+    def get_last_status(self, obj):
+        verificoes = obj.verificacao_set.all().order_by('-created_at')
+        if verificoes.count() == 0:
+            return 'N/A'
+        else:
+            return verificoes[0].get_status_display()
+    get_last_status.short_description = 'Last Status'
+
 
 
 @admin.register(Verificacao)
 class VerificacaoModelAdmin(admin.ModelAdmin):
     search_fields = ['endereco', ]
-    list_display = ['get_uuid', 'endereco', 'status']
+    list_display = ['get_uuid', 'endereco', 'created_at', 'status']
